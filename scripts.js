c71367a6e8030d4aea4ab9ab0a96468276aa553d
Odoyor Eyeniyan <56475386+peejaycode@users.noreply.github.com>
2024-05-25 00:55:47 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // JavaScript code can be added here for interactive features
+    console.log('Document is fully loaded and parsed');
+});
